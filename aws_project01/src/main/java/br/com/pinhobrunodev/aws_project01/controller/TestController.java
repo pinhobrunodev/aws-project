ecfe75b91b1c0d896b8b6ece7786b3ffc16b8049
pinhobrunodev <iaishowcs@gmail.com>
2022-07-12 22:56:12 -0300

    Project created and 01 on dockerhub

---
+++ aws_project01/src/main/java/br/com/pinhobrunodev/aws_project01/controller/TestController.java
@@ -0,0 +1,26 @@
+package br.com.pinhobrunodev.aws_project01.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/test")
+public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+
+    @GetMapping(value = "/dog/{name}")
+    public ResponseEntity<?> dogTest(@PathVariable String name)
+    {
+        LOG.info("Test controller - name: {}",name);
+        return ResponseEntity.ok("Name: "+name);
+    }
+
+
+}
